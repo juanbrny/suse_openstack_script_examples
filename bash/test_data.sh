86a74578e52e2800d557b429a7349db5bd94c8fe
Juan Herrera <juan.herrera@suse.com>
2018-12-05 19:54:29 +0100

    Update test_data.sh

--- bash/test_data.sh
+++ bash/test_data.sh
@@ -4,4 +4,4 @@ export DEMO_PROJECT_NAME="acmetest"
 export DEMO_DEFAULT_USR_PWD="1234acme"
 export DEMO_NUMUSERS=2
 
-DEMO_IMAGE="../resources/image/cirros-0.4.0-x86_64-disk.img"
+export DEMO_IMAGE="../resources/image/cirros-0.4.0-x86_64-disk.img"
